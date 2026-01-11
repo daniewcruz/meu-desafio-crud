e3dae0d0c2977c856bdca6fa58fa2076b8f16faf
Daniel Lucas da Silva Cruz <156431265+daniewcruz@users.noreply.github.com>
2026-01-11 15:59:08 -0300

    ajustes footer
    Removed footer text related to project description.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,8 +100,6 @@ export default function Home() {
       {/* Footer */}
       <footer className="footer footer-center p-6 md:p-10 text-base-content">
         <div>
-          <p className="font-semibold text-base md:text-lg">Desafio CRUD</p>
-          <p className="text-xs md:text-sm opacity-70">Desenvolvido com Next.js, React, TypeScript, Tailwind CSS, DaisyUI e PostgreSQL</p>
           <p className="text-xs opacity-50 mt-1 md:mt-2">2026 - Daniew Cruz</p>
         </div>
       </footer>
